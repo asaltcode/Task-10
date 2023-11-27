723c10d7743d59f9c2c841710bfd753f2fb0cd90
asaltcode <elangovanr558@gmail.com>
2023-11-27 11:26:02 +0530

    task comlete

---
+++ script.js
@@ -0,0 +1,35 @@
+let h1 = document.getElementById("num");
+
+setTimeout(()=>{
+    h1.innerText = "10";
+    setTimeout(()=>{
+        h1.innerText = "9";
+        setTimeout(()=>{
+            h1.innerText = "8";
+            setTimeout(()=>{
+                h1.innerText = "7";
+                setTimeout(()=>{
+                    h1.innerText = "6";
+                    setTimeout(()=>{
+                        h1.innerText = "5";
+                        setTimeout(()=>{
+                            h1.innerText = "4";
+                            setTimeout(()=>{
+                                h1.innerText = "3";
+                                setTimeout(()=>{
+                                    h1.innerText = "2";
+                                    setTimeout(()=>{
+                                        h1.innerText = "1";
+                                        setTimeout(()=>{
+                                            h1.innerText = "Happy Independence Day";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
